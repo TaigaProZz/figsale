89d12fc3eee4eeeb4e2001a69edcafd30d223c49
Florian <74510014+TaigaProZz@users.noreply.github.com>
2023-08-20 01:52:10 +0200

    product page started, navbar logo fixes,

---
+++ figsale/src/components/products-page/products-list/ProductsList.jsx
@@ -0,0 +1,11 @@
+import './ProductsList.scss';
+
+function ProductsList() {
+  return (
+    <div className='products-list-container'>
+      list
+    </div>
+  )
+}
+
+export default ProductsList;
