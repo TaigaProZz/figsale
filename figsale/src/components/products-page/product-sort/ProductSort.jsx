7b23aaf3a7db8c45a021ce026ef2e03404b73486
Florian <74510014+TaigaProZz@users.noreply.github.com>
2023-08-21 18:30:42 +0200

    product page responsive ( not filter for mobile ), sticky navbar for large screen

--- figsale/src/components/products-page/product-sort/ProductSort.jsx
+++ figsale/src/components/products-page/product-sort/ProductSort.jsx
@@ -1,23 +1,78 @@
+import 'rc-slider/assets/index.css';
 import './ProductSort.scss';
+import Slider from 'rc-slider';
+import { useState } from 'react';
+
+const licence = [
+  "Licence 1",
+  "Licence 2",
+  "Licence 3",
+  "Licence 4",
+  "Licence 5",
+  "Licence 6",
+  "Licence 7",
+];
+
+const size = [
+  10,
+  20,
+  30,
+  50,
+  100,
+  200
+];
+
 
 function Sort () {
+  const [sliderValues, setSliderValues] = useState([0, 1000]);
+
+  const handleSliderChange = (values) => {
+    console.log(values);
+    setSliderValues(values);
+  };
+
   return (
     <div className='product-sort-container'>
       <div className='product-sort-shape'>
         <div className='product-sort-shape-title'>
           <h2>Trier par</h2>
         </div>
-        <div className='product-sort-shape-price'>
+        <div className='product-sort-price-container'>
           <h3>Prix</h3>
-          <div className='product-sort-shape-price-input-container'>
-            <div>
-              <label>Minimum</label>
-              <input className='product-sort-shape-price-input' type='number' placeholder='€' />
+          <div className='product-sort-price-slider-container'>
+            <div className='price-slider'>
+              <p>
+                {sliderValues.map(value => value + "€").join(' - ')}
+              </p>
+              <Slider 
+                range={true}
+                allowCross={false}
+                min={0} 
+                max={1000} 
+                defaultValue={sliderValues} 
+                onChange={handleSliderChange} 
+              />
             </div>
-            <div>
-              <label>Maximum</label>
-              <input className='product-sort-shape-price-input' type='number' placeholder='€' />
-            </div>
+          </div>
+        </div>
+        <div className='product-sort-licence-container'>
+          <h3>Licence</h3>
+          <div className='product-sort-licence-select-container'>
+            <select>
+              {licence.map((licence, index) => (
+                <option key={index} value={licence}>{licence}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className='product-sort-size-container'>
+          <h3>Taille</h3>
+          <div className='product-sort-size-select-container'>
+            <select>
+              {size.map((size, index) => (
+                <option key={index} value={size}>{size}</option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
